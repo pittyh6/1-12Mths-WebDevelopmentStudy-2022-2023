db1135be4ba30a1d93e3e19ce9adacf0ada76fc2
Priscila Ferreira Bezerra <pittyh6@gmail.com>
2022-06-29 15:30:06 +1000

    [wds-31] done

--- WDS-31_ES6-8_JavaScript_ES6-ES2015_08_Set,_Map,_WeakSet_and_WeakMap/README.md
+++ WDS-31_ES6-8_JavaScript_ES6-ES2015_08_Set,_Map,_WeakSet_and_WeakMap/README.md
@@ -4,10 +4,37 @@
 ![Begin Banner](Documentation/top-1200x350.gif)
  
 # Result
-![Middle Banner](/WDS-31_ES6-8_JavaScript_ES6-ES2015_08_Set,_Map,_WeakSet_and_WeakMap/wds-30.png)
+![Middle Banner](/WDS-31_ES6-8_JavaScript_ES6-ES2015_08_Set,_Map,_WeakSet_and_WeakMap/wds-31.png)
    
 ## What I learned/used
 * ES6
+    * Set
+        * Set object = store unique values
+        * Methods
+            * add()
+            * delete()
+            * clear()
+        * size
+        * loop
+            * forEach()
+    * Map
+        * create => ... = new Map([])
+        * Methods
+            * set
+            * delete
+        * size
+    * WeakSet
+        * add()
+        * delete()
+    * WeakMap
+        * create => ... = new Map()
+        * set()
+        * delete()
+
+
+
+
+
 
 
 
--- WDS-31_ES6-8_JavaScript_ES6-ES2015_08_Set,_Map,_WeakSet_and_WeakMap/main.js
+++ WDS-31_ES6-8_JavaScript_ES6-ES2015_08_Set,_Map,_WeakSet_and_WeakMap/main.js
@@ -1,42 +1,83 @@
-function greet ($greeting){
-    console.log($greeting);
+//SET
+let myArray = [11,22,34,56,7];
+let mySet = new Set(myArray);
+
+mySet.add('100');
+mySet.add({a: 1, b:2});
+mySet.delete('100');
+mySet.clear();
+mySet.add('100');
+mySet.add({a: 1, b:2});
+mySet.add(11);
+
+console.log(mySet);
+console.log(mySet.size);
+
+mySet.forEach(function(val){
+    console.log(val);
+})
+
+//MAP//
+/*
+The Map object holds key-value pairs and remembers the original insertion order of the keys. 
+Any value (both objects and primitive values) may be used as either a key or a value.
+*/
+let myMap = new Map([['a1','hello'], ['a2','world']]);
+myMap.set('c3', 'bye world');
+myMap.delete('a1')
+console.log(myMap);
+console.log(myMap.size);
+
+//WeakSet
+/*The WeakSet object lets you store weakly held objects in a collection.
+Description
+WeakSet objects are collections of objects. Just as with Sets, 
+each object in a WeakSet may occur only once; all objects in a WeakSet's collection are unique.
+*/
+
+let carWeakSet = new WeakSet();
+console.log(carWeakSet);
+
+let car1 = {
+    make: 'Honda',
+    model: 'Civic'
 }
-greet(); //undefined -> error
-greet('Hi baby'); //Hi baby
-//with default parameter
-function greet2 ($greeting = "Hello Hello!!!"){
-    console.log($greeting);
+let car2 = {
+    make: 'Fiat',
+    model: 'Uno'
 }
-greet2(); //Hello Hello!!! -> default parameter
-greet2('Hi baby');//Hi baby
+carWeakSet.add(car1);
+carWeakSet.add(car2);
+carWeakSet.delete(car1);
+
+console.log(carWeakSet);
 
 
+//WEAKMAP
+/*A WeakMap is a collection of key/value pairs whose keys must be objects, 
+with values of any arbitrary JavaScript type, and which does not create strong references 
+to its keys. That is, an object's presence as a key in a WeakMap does not prevent the object from 
+being garbage collected. Once an object used as a key has been collected, 
+its corresponding values in any WeakMap become candidates for garbage collection as well — 
+as long as they aren't strongly referred to elsewhere.*/
 
-// Spread Operator
-let args = [1,2,3];
-let args2 = [4,5,6]
-function test(){
-    console.log(args);
+let carWeakMap = new WeakMap();
+let key1 = {
+    id: 1
 }
-function test2(){
-    console.log(args + ',' +args2);
+let car3 = {
+    make: 'Honda',
+    model: 'Civic'
 }
-test(); 
-test(...args);
-test2(...args, ...args2);
+let key2 = {
+    id: 2
+}
+let car4 = {
+    make: 'Fiat',
+    model: 'Uno'
+}
+carWeakMap.set(key1, car3);
+carWeakMap.set(key2, car4);
 
-
-/** Spread syntax (...) allows an iterable such as an array expression or string 
- * to be expanded in places where zero or more arguments (for function calls) or 
- * elements (for array literals) are expected, or an object expression 
- * to be expanded in places where zero or more key-value pairs 
- * (for object literals) are expected.
-        function sum(x, y, z) {
-        return x + y + z;
-        }
-
-        const numbers = [1, 2, 3];
-
-        console.log(sum(...numbers));
-        // expected output: 6
- */
+carWeakMap.delete(key1);
+console.log(carWeakMap);
